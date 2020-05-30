4808eab6b9688a3268cc4d89efbac65ba00aaa8a
Hiroki Tanaka <hiroki.tanaka@kumano-te.com>
2020-05-30 22:30:47 +0900

    feat: #5

---
+++ client/layouts/centered.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-app dark>
+    <v-content class="grey lighten-5">
+      <v-container class="fill-height" fluid>
+        <nuxt />
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
---
+++ client/store/index.js
@@ -0,0 +1,1 @@
+// to prevent @auth/module error
